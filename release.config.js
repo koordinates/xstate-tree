3eab46e81e4278f17f2d2561b1923c014322aa3e
Taylor Lodge <taylor.lodge@koordinates.com>
2024-01-10 14:37:29 +1300

    chore(release): update it on next

--- release.config.js
+++ release.config.js
@@ -2,6 +2,7 @@ const base = require("semantic-release-npm-github-publish");
 
 module.exports = {
   ...base,
+  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'master', {name: 'next', prerelease: true}, {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
   releaseRules: [],
   plugins: [
     ...base.plugins.slice(1),
